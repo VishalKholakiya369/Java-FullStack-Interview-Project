5469b19dbc6511b638c24c932cc56c155c0ae5a8
Vishal Kholakiya <vishalkholakiya369@gmail.com>
2025-07-26 10:57:00 +0530

    Added backend and frontend folder structure

---
+++ backend/src/main/java/com/interview/javainterviewproject/JavainterviewprojectApplication.java
@@ -0,0 +1,13 @@
+package com.interview.javainterviewproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavainterviewprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavainterviewprojectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/interview/javainterviewproject/JavainterviewprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.javainterviewproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavainterviewprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
